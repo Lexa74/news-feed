3d2d8c59bb05e3b5998987c7c25879e09d9936f6
Sealexey <tany--96@mail.re>
2023-08-19 17:03:29 +0300

    Вернул

--- src/services/newsApi.ts
+++ src/services/newsApi.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const apiKey = ''
+const apiKey = '44e76e190e004d17a52ba3892afefc2d'
 
 export const getAllNews = async (sortBy: string = '') => {
     const response = await axios.get(`https://newsapi.org/v2/top-headlines?country=us&sortBy=${sortBy}&apiKey=${apiKey}`)
